ee4d8dd89e2d8d4be3ecd8cd4c5f67a79653361d
Jefti Vinicius Meira Dantas <1282420838@ulife.com.br>
2025-08-29 21:03:52 -0300

    dados

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i = 5, x = 6;
+
+string nome = "João da Silva";
+
+string veiculo = "Corolla";
+
+string resultado = $"O veiculo de {nome} é um {veiculo}";
+
+Console.WriteLine(resultado);
